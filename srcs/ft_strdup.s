0652afb61d555238b632297e8c4b3d4b3dc3e0b3
Sami DARNAUD <samidarnaud@gmail.com>
2020-01-29 11:30:32 +0100

    commenting ft_strdup

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -4,21 +4,21 @@ extern	_malloc
 extern	_ft_strlen
 extern	_ft_strcpy
 
-_ft_strdup:
-	cmp		rdi, 0
+_ft_strdup:					;rdi = str
+	cmp		rdi, 0			;check if == NULL
+	jz		error			;go to error
+	call	_ft_strlen		;rax = len(str)
+	push	rdi				;save rdi
+	inc		rax				;rax = len + 1 for '\0'
+	mov		rdi, rax		;put rax in rdi to call malloc
+	call	_malloc			;malloc(rax)
+	cmp		rax, 0			;check the malloc
 	jz		error
-	call	_ft_strlen
-	push	rdi
-	inc		rax
-	mov		rdi, rax
-	call	_malloc
-	cmp		rax, 0
-	jz		error
-	pop		rdi
-	mov		rsi, rdi
-	mov		rdi, rax
-	call	_ft_strcpy
-	cmp		rax, 0
+	pop		rdi				;take back rdi from stack
+	mov		rsi, rdi		;put rdi in rsi (src)
+	mov		rdi, rax		;put the allocate memory create by malloc in rdi (dst)
+	call	_ft_strcpy		;call strcpy
+	cmp		rax, 0			;check if strcpy went wrong
 	jz		error
 	ret
 
