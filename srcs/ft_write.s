dd37ee063cf6a8e8766c0401dd887f81fab7fa8b
samphoto <samidarnaud@gmail.com>
2020-07-06 15:16:05 +0200

    errno_location

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,14 +1,20 @@
 section	.text
 global	ft_write
+extern	__errno_location
 
 ;rdi first arg (fd), rsi second arg (*buf) and rdx third arg (nbytes)
 
 ft_write:
-	mov	rax, 1		;write= 4 and MacOX = 0x2000000 ?
+	mov	rax, 1				;write= 4 and MacOX = 0x2000000 ?
 	syscall					;system call
-	jc error				;if CF (carry flag), then error
+	cmp	rax, 0
+	jl erreur				;if CF (carry flag), then error
 	ret
 
-error:
+erreur:
+	neg	rax
+	mov	rdi, rax
+	call __errno_location
+	mov [rax], rdi
 	mov rax, -1
 	ret
